2b92f8670e0d154c6e075e07cdc7f461e0e4617f
Eduardo Czamanski Rota <eduardoczamanski@protonmail.com>
2023-06-03 14:46:31 -0300

    feat: adicionado segurança a rotas de criação de usuario

--- api/apps/v1/usuario/routes.py
+++ api/apps/v1/usuario/routes.py
@@ -4,35 +4,51 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import UUID4
 from sqlalchemy.orm import Session
 
+from api.apps.v1.login.services import validate_token
 from api.apps.v1.usuario import schemas, services
 from api.config.settings import settings
+from api.core.auth import reuseable_oauth
 from api.core.database import get_db
 
 router = APIRouter(prefix="/usuario")
 
 
 @router.post("/", response_model=schemas.Usuario)
-async def create_usuario(usuario: schemas.Usuario, db: Session = Depends(get_db)):
-    exists = services.get_usuario_email(db=db, email=usuario.email)
+async def create_usuario(
+    usuario: schemas.Usuario,
+    db: Session = Depends(get_db),
+    token: str = Depends(reuseable_oauth),
+):
+    await validate_token(token)
+    exists = await services.get_usuario(db=db, username=usuario.email)
     if exists:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Usuário com este email já existe",
         )
-    return services.create_usuario(db=db, usuario=usuario)
+    return await services.create_usuario(db=db, usuario=usuario)
 
 
 @router.get("/", response_model=List[schemas.Usuario])
 async def list_usuarios(
-    offset: int = 0, limit: int = 100, db: Session = Depends(get_db)
+    offset: int = 0,
+    limit: int = 100,
+    db: Session = Depends(get_db),
+    token: str = Depends(reuseable_oauth),
 ):
-    usuarios = services.get_usuarios(db, offset=offset, limit=limit)
+    await validate_token(token)
+    usuarios = await services.get_usuarios(db, offset=offset, limit=limit)
     return usuarios
 
 
 @router.get("/{usuario_id}", response_model=schemas.Usuario)
-async def read_usuario(usuario_id: UUID4, db: Session = Depends(get_db)):
-    db_usuario = services.get_usuario(db, usuario_id=usuario_id)
+async def read_usuario(
+    usuario_id: UUID4,
+    db: Session = Depends(get_db),
+    token: str = Depends(reuseable_oauth),
+):
+    await validate_token(token)
+    db_usuario = await services.get_usuario_by_id(db, usuario_id=usuario_id)
     if db_usuario is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado"
--- api/apps/v1/usuario/services.py
+++ api/apps/v1/usuario/services.py
@@ -1,24 +1,29 @@
 from sqlalchemy.orm import Session
 
 from api.apps.v1.usuario import models, schemas
-from api.config.auth import get_hashed_password
+from api.core.auth import get_hashed_password
 
 
-async def get_usuario(db: Session, usuario_id: int):
+async def get_usuario(db: Session, username: int):
+    email = db.query(models.Usuario).filter_by(email=username).first()
+    if email:
+        return email
+    return db.query(models.Usuario).filter_by(username=username).first()
+
+
+async def get_usuario_by_id(db: Session, usuario_id: int):
     return db.query(models.Usuario).get(usuario_id)
 
 
-async def get_usuario_email(db: Session, email: str):
-    return db.query(models.Usuario).get(email)
-
-
-async def get_usuarios(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Usuario).offset(skip).limit(limit).all()
+async def get_usuarios(db: Session, offset: int = 0, limit: int = 100):
+    return db.query(models.Usuario).offset(offset).limit(limit).all()
 
 
 async def create_usuario(db: Session, usuario: schemas.Usuario):
     db_usuario = models.Usuario(
-        username=usuario.username, password=get_hashed_password(usuario.password)
+        email=usuario.email,
+        username=usuario.username,
+        password=get_hashed_password(usuario.password.get_secret_value()),
     )
     db.add(db_usuario)
     db.commit()
